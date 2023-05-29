eb29e2f1292a22a6350275eac981780722211d7c
Noah S <Noah3141@gmail.com>
2023-05-29 09:16:51 -0400

    first render of db data

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,10 +12,10 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export default withClerkMiddleware((req: NextRequest) => {
-  return NextResponse.next();
+    return NextResponse.next();
 });
 
 // Stop Middleware running on static files
 export const config = {
-  matcher: "/((?!_next/image|_next/static|favicon.ico).*)",
+    matcher: "/((?!_next/image|_next/static|favicon.ico).*)",
 };
